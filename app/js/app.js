8e268f045275c00e36d110cf52155d20d16fba78
iancatania <78445705+iancatania@users.noreply.github.com>
2021-05-12 11:46:23 -0400

    evenDiamond rough draft JS

--- app/js/app.js
+++ app/js/app.js
@@ -30,25 +30,57 @@ let createOddDiamond = (diamond) => {
 
 let createEvenDiamond = (diamond) => {
 
+    let diamondPrint = ``;
+
+    for (let length = 1; length <= diamond + 1; length += 2) {
+
+        for (let height = 0; height < length; height++) {
+
+            diamondPrint += `*`;
+
+        }
+
+        diamondPrint += `<br>`;
+    }
+
+    for (let length = diamond - 1; length > 0; length -= 2) {
+
+        for (let height = 0; height < length; height++) {
+
+            diamondPrint += `*`;
+
+        }
+
+        diamondPrint += `<br>`;
+    }
+
+    document.querySelector('div').innerHTML = diamondPrint;
+
 };
 
 let diamondAnimate = (diamondSet) => {
 
-      let pos = 1;
-      let left = diamondSet.offsetLeft;
-      let windowSize = window.innerWidth;
-      let width = diamondSet.offsetWidth;
+    let pos = 1;
+    let left = diamondSet.offsetLeft;
+    let windowSize = window.innerWidth;
+    let width = diamondSet.offsetWidth;
 
-      if (left > windowSize - width) {
-          pos = -1;
-      }
+    if (left > windowSize - width) {
 
-      if (left < 0) {
-          pos = 1;
-      }
+        pos = -1;
 
-      left += pos;
-      diamondSet.style.left = left + `px`;
+    }
+
+    if (left < 0) {
+
+        pos = 1;
+
+    }
+
+    left += pos;
+
+    diamondSet.style.left = left + `px`;
+
 };
 
 
@@ -58,14 +90,20 @@ window.onload = () => {
     let diamondSet = document.querySelector('div');
 
     if (diamond % 2 != 0) {
+
       createOddDiamond(diamond);
+
     }
 
     if (diamond % 2 == 0) {
+
       createEvenDiamond(diamond);
+
     }
 
     setInterval(() => {
+      
       diamondAnimate(diamondSet);
-    }, 1); //interval for animation set at 1)
+
+    }, 1); //interval for animation set at 1 ms)
 };
