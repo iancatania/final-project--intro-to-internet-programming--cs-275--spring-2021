851cfbb66053b654c8fc83f96b9cedae2f851740
iancatania <78445705+iancatania@users.noreply.github.com>
2021-05-12 09:37:40 -0400

    oddDiamond rough draft JS

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,52 @@
+let createOddDiamond = (diamond) => {
+
+  let diamondPrint = ``;
+
+  for (let length = 1; length <= diamond; length += 2) {
+
+      for (let height = 0; height < length; height++) {
+
+          diamondPrint += `*`;
+
+      }
+
+      diamondPrint += `<br>`;      
+  }
+
+  for (let length = diamond - 2; length > 0; length -= 2) {
+
+      for (let height = 0; height < length; height++) {
+
+          diamondPrint += `*`;
+
+      }
+
+      diamondPrint += `<br>`;
+  }
+
+  document.querySelector('div').innerHTML = diamondPrint;
+
+};
+
+let createEvenDiamond = (diamond) => {
+
+};
+
+let diamondAnimate = () => {
+
+};
+
 window.onload = () => {
-    console.log(`%c JavaScript works!`,
-        `background-color: yellow; font-weight: bold;`);
+
+    let diamond = parseInt(prompt('Enter diamond size as a number', '0'));
+
+    if (diamond % 2 != 0) {
+      createOddDiamond(diamond);
+    }
+
+    else {
+      createEvenDiamond(diamond);
+    }
+
+    diamondAnimate;
 };
