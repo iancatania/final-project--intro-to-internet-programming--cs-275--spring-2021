d827608a3c512f450987441ef72a5a6ab9992849
iancatania <78445705+iancatania@users.noreply.github.com>
2021-05-12 22:29:00 -0400

    Fixed bug with even diamond

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
-let createDiamond = (diamond) => {
+let createDiamond = (diamond) => { //function to generate diamond
 
-  let diamondPrint = ``;
+    let diamondPrint = ''; //empty string to store diamond content
 
-  if (diamond % 2 != 0) {
+    if (diamond % 2 !== 0) { //if the size input is odd
     for (let length = 1; length <= diamond; length += 2) {
         for (let height = 0; height < length; height++) {
             diamondPrint += `*`;
@@ -15,58 +15,61 @@ let createDiamond = (diamond) => {
             diamondPrint += `*`;
         }
         diamondPrint += `<br>`;
+        document.querySelector('div').style.lineHeight = '60%'; //changes line style height for odd diamonds
     }
-  }
+}
 
-  else if (diamond % 2 == 0) {
+    else {
 
-    let diamondPrint = ``;
+        diamondPrint = ' *'; //extra asterisk for even
 
-    for (let length = 1; length <= diamond + 1; length += 2) {
-        for (let height = 0; height < length; height++) {
-            diamondPrint += `*`;
+        for (let length = 0; length <= diamond + 1; length += 2) {
+            for (let height = 0; height < length; height++) {
+                diamondPrint += `* `;
+            }
+            diamondPrint += `<br>`;
         }
-        diamondPrint += `<br>`;
-    }
 
-    for (let length = diamond - 1; length > 0; length -= 2) {
-        for (let height = 0; height < length; height++) {
-            diamondPrint += `*`;
-        }
+        for (let length = diamond - 2; length > 0; length -= 2) {
+            for (let height = 0; height < length; height++) {
+                diamondPrint += `* `;
+            }
         diamondPrint += `<br>`;
+        }
+        diamondPrint += ' *';
     }
-  }
-  document.querySelector('div').innerHTML = diamondPrint;
+    document.querySelector('div').innerHTML = diamondPrint; //sets the innerHTML of the div to the string content of diamondPrint
 };
 
-window.onload = () => {
+window.onload = () => { //everything that is loaded when page loads
 
     let diamond = parseInt(prompt('Enter diamond size as a number', '0'));
     let diamondSet = document.querySelector('div');
 
-    if (diamond > 0) {
-      createDiamond(diamond);
+    createDiamond(diamond); //function call
+
+    if (diamond < 0 || isNaN(diamond)) { //if not a valid number, reload
+        window.location.reload();
     }
 
     let pos = 1;
     let left = diamondSet.offsetLeft;
 
-    setInterval(() => {
+    setInterval(() => { //animates diamond-container div
 
-      let windowSize = window.innerWidth;
-      let width = diamondSet.offsetWidth;
+        let windowSize = window.innerWidth; //gets width of window
+        let width = diamondSet.offsetWidth; //gets width of diamond
 
-      if (left > windowSize - width) {
-          pos = -1;
-      }
+        if (left > windowSize - width) { //moves diamond
+            pos = -1;
+        }
 
-      if (left < 0) {
-          pos = 1;
-      }
+        if (left < 0) {
+            pos = 1;
+        }
 
-      left += pos;
-
-      diamondSet.style.left = left + `px`;
+        left += pos;
+        diamondSet.style.left = left + `px`;
 
     }, 1); //interval for animation set at 1 ms)
 };
